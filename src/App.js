9eba57293ac99d6ef5d1c8e32986a1838b5c16d1
CL Schalkwyk <clschalkwyk@gmail.com>
2020-07-25 00:53:44 +0200

    init repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Navigation from './section/Navigation';
+import Home from './page/Home';
+import Signup from './page/Signup';
+import Signin from './page/Signin';
+import Footer from './section/Footer';
 
 function App() {
   return (
-    <div className="App">
-    Hllo
-    </div>
+    <Router>
+      <Navigation/>
+      <div className='container'>
+      <Switch>
+        <Route path='/join'>
+          <Signup/>
+        </Route>
+        <Route path='/signin'>
+          <Signin/>
+        </Route>
+        <Route path='/'>
+          <Home/>
+        </Route>
+      </Switch>
+      </div>
+      <Footer/>
+    </Router>
   );
 }
 
