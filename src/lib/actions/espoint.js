7177c8584187bf3dd93beab035ca6d46add50c44
CL Schalkwyk <clschalkwyk@gmail.com>
2020-08-10 18:26:25 +0200

    added ES endpoint for homepage

---
+++ src/lib/actions/espoint.js
@@ -0,0 +1,23 @@
+import Axios from 'axios';
+import {hasToken, getToken} from '../token';
+
+let esPoint;
+// if(hasToken()){
+//   esPoint = Axios.create({
+//     baseURL: process.env.REACT_APP_ES_ENDPOINT,
+//     headers: {
+//       'content-type': 'application/json',
+//       'Authorization': `Bearer ${getToken()}`
+//     }
+//   });
+// }else{
+  esPoint = Axios.create({
+    baseURL: process.env.REACT_APP_ES_ENDPOINT,
+    headers: {
+      'content-type': 'application/json',
+    },
+
+  });
+// }
+
+export default esPoint;
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AdvertCard from './components/AdvertCard';
-
+import esPoint from '../lib/actions/espoint';
 import {useCookies} from 'react-cookie';
 
 function Home(){
 
   const [cookies, setCookie] = useCookies(['token']);
   console.log(cookies);
+
+  const [homeAds, setHomeAds] = useState([]);
+
+  useEffect(() =>{
+    (async () => {
+      const ads = await esPoint.get('/browse');
+      setHomeAds(ads?.data);
+      console.log(ads.data);
+    })()
+  },[]);
 
   return (
       <section>
